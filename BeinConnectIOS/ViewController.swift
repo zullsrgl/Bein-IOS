36d821466806f039da39f23554ef9f92bbea0864
zulalsarioglu <zulal.sariogluu@gmail.com>
2023-11-07 10:51:19 +0300

    Initial Commit

---
+++ BeinConnectIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeinConnectIOS
+//
+//  Created by Zülal Sarıoğlu on 7.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
