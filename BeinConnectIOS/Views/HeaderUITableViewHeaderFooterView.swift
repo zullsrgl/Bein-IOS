08da8278f0d5ae12d5f92ae0dabfda9baecd5901
zulalsarioglu <zulal.sariogluu@gmail.com>
2023-11-16 11:23:34 +0300

    Navigation Controller

---
+++ BeinConnectIOS/Views/HeaderUITableViewHeaderFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderUITableViewHeaderFooterView.swift
+//  BeinConnectIOS
+//
+//  Created by Zülal Sarıoğlu on 14.11.2023.
+//
+
+import UIKit
+
+class HeaderUITableViewHeaderFooterView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
